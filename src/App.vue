a4a4fa8c0f56752731c4c5ccf3697b503ecc129c
Marcap00 <cappellatomarco@yahoo.com>
2024-11-30 22:26:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+
+// import AppHeader from './components/AppHeader.vue'
+
+import AppMain from './components/AppMain.vue'
+
+// import AppBannerIcons from './components/AppBannerIcons.vue';
+
+// import AppFooter from './components/AppFooter.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    // AppHeader,
+    AppMain,
+    // AppBannerIcons,
+    // AppFooter
+  }
+}
+
+</script>
+
+<template>
+
+  <!-- <AppHeader /> -->
+  <AppMain />
+  <!-- <AppBannerIcons /> -->
+  <!-- <AppFooter /> -->
+
+</template>
+
+<style lang="scss">
+@use './styles/main.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppMain",
+    components: {},
+    props: {},
+    data() {
+        return {
+
+        };
+    },
+    methods: {},
+    computed: {},
+};
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
